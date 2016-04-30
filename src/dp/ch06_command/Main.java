f0d1592a5a517f777471c04f1f930cebe56f0894
ss <test@test.com>
2016-05-01 03:42:00 +0900

    05/01

--- src/dp/ch06_command/Main.java
+++ src/dp/ch06_command/Main.java
@@ -10,7 +10,7 @@ public class Main {
     // Client・・・Commandを作成
     // Command・・・execute()でコマンドを実行。undo()でコマンドを取り消し？
     // Invoker・・・set[add]Command()でCommandを保持（複数）し、invoke()により保持したコマンドを実行
-    // Receiver・・・？
+    // Receiver・・・Commandが処理実行時の操作を移譲
     public static void main(String... args){
         Client client = new Client();
         Invoker invoker = new Invoker();
--- src/dp/ch07_adaptor/Main.java
+++ src/dp/ch07_adaptor/Main.java
@@ -4,6 +4,11 @@ package dp.ch07_adaptor;
  *  2016/03/27.
  */
 public class Main {
+    // 使うInterfaceが固定されているが、別のInterfaceを持つ処理を流用したい場合に
+    // Interfaceを変えずに解決する方法
+    //
+    // Adapter・・・使うことが決まっているInterfaceを実装したクラス
+    // Adaptee・・・本来使いたい別のInterfaceを実装したクラス
     public static void main(String... args){
         Adapter adapter = new Adapter(new Adaptee());
         adapter.doSomething();
